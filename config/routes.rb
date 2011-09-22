65ce1c74c9afb381e80c1c97dfdbb878474b5bdd
bhauman <bhauman@gmail.com>
2011-09-21 23:18:15 -0700

    adding a link to get an overall report

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   namespace :cohortly do
+    match     'report_all', :controller => 'reports', :action => 'show', :as => :report_all
     resources :metrics, :only => [:index]
     resources :reports, :only => [:index, :show]
   end
