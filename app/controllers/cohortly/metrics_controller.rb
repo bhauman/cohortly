8237559e5e39eed63aad2150b50f0a28b01cfa52
bhauman <bhauman@gmail.com>
2011-10-04 22:28:34 -0700

    adding pagination to metrics page

--- app/controllers/cohortly/metrics_controller.rb
+++ app/controllers/cohortly/metrics_controller.rb
@@ -2,13 +2,13 @@ class Cohortly::MetricsController < Cohortly::CohortlyController
   def index
     @metric_search = Cohortly::Metric.new(params[:cohortly_metric])
     
-    scope = Cohortly::Metric.limit(250).sort(:created_at.desc)
-    if params[:tags]
-      scope = scope.where(:tags => { :$all => params[:tags] })
+    scope = Cohortly::Metric.sort(:created_at.desc)
+    if params[:cohortly_metric] && params[:cohortly_metric][:tags]
+      scope = scope.where(:tags => { :$all => @metric_search.tags })
     end
     if @metric_search.user_id
       scope = scope.where(:user_id => @metric_search.user_id)
     end
-    @metrics = scope.all
+    @metrics = scope.paginate(:per_page => 200, :page => params[:page])
   end
 end
